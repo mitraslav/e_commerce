3fc1da6fc4c5e5c0a972bdaae5d71a94a64ebfae
slavaekim <dmitryraskoln@gmail.com>
2025-08-22 13:27:39 +0300

    product and category classes created and tested

---
+++ tests/test_category.py
@@ -0,0 +1,10 @@
+
+def test_init(category_phones, category_tvs):
+    assert category_phones.name == 'Phones'
+    assert category_phones.description == 'You can call your friends'
+
+    assert category_phones.category_count == 2
+    assert category_tvs.category_count == 2
+
+    assert category_phones.product_count == 5
+    assert category_tvs.product_count == 5
---
+++ tests/test_product.py
@@ -0,0 +1,7 @@
+from tests.conftest import product_phone
+
+def test_init(product_phone):
+    assert product_phone.name == 'Samsung'
+    assert product_phone.description == 'You can call your friends'
+    assert product_phone.price == 100000.0
+    assert product_phone.quantity == 4
