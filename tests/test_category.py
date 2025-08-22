7b2c9ebea237b5989db4c24e174d0428f25e3b75
slavaekim <dmitryraskoln@gmail.com>
2025-08-22 14:30:25 +0300

    final fixes

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,7 +1,6 @@
-
 def test_init(category_phones, category_tvs):
-    assert category_phones.name == 'Phones'
-    assert category_phones.description == 'You can call your friends'
+    assert category_phones.name == "Phones"
+    assert category_phones.description == "You can call your friends"
 
     assert category_phones.category_count == 2
     assert category_tvs.category_count == 2
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,7 +1,5 @@
-from tests.conftest import product_phone
-
 def test_init(product_phone):
-    assert product_phone.name == 'Samsung'
-    assert product_phone.description == 'You can call your friends'
+    assert product_phone.name == "Samsung"
+    assert product_phone.description == "You can call your friends"
     assert product_phone.price == 100000.0
     assert product_phone.quantity == 4
