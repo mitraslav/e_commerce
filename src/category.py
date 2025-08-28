34fa9383a4c1d30f5d1ba0a2b3c7a04ac087e11f
slavaekim <dmitryraskoln@gmail.com>
2025-08-28 16:41:33 +0300

    category class add dockstring

--- src/category.py
+++ src/category.py
@@ -2,12 +2,29 @@ from src.product import Product
 
 
 class Category:
+    """Класс отслеживает общее количество категорий и товаров во всех категориях,
+    а также предоставляет методы для работы с товарами конкретной категории.
+
+    Attributes:
+        name (str): Название категории
+        description (str): Описание категории
+        category_count (int): Счетчик общего количества созданных категорий (классовый атрибут)
+        product_count (int): Счетчик общего количества товаров во всех категориях (классовый атрибут)
+    """
+
     name: str
     description: str
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
+        """Инициализирует экземпляр категории товаров.
+
+        Args:
+            name (str): Название категории
+            description (str): Описание категории
+            products (list): Список объектов товаров (класс Product), принадлежащих категории
+        """
         self.name = name
         self.description = description
         self.__products = products if products else []
@@ -15,11 +32,24 @@ class Category:
         Category.product_count += len(products) if products else 0
 
     def add_product(self, product: Product):
+        """Добавляет товар в категорию.
+        Args:
+            product(Product): Объект товара для добавления в категорию
+
+        Increases:
+            Увеличивает общий счетчик товаров (product_count) на 1
+        """
         self.__products.append(product)
         Category.product_count += 1
 
     @property
     def products(self):
+        """Возвращает форматированную строку с информацией о товарах категории.
+
+        Returns:
+            str: Строка с информацией о каждом товаре в формате: '<Название продукта>, <цена продукта> руб. Остаток: <количество>.'
+                Каждый товар на отдельной строке.
+        """
         product_list = []
         for product in self.__products:
             product_list.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity}.")
